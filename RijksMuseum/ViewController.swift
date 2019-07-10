5ed7332208d0f0bf082e7968141bc970ca996d7f
Adam Lovastyik <adam@lovastyik.hu>
2019-07-10 15:12:43 +0200

    Initial Commit

---
+++ RijksMuseum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RijksMuseum
+//
+//  Created by Adam Lovastyik on 10/07/2019.
+//  Copyright © 2019 Adam Lovastyik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
